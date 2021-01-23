e4057bb24a7913da9c6d2aa6b9340ab19ce19712
Pedro Rodrigues <pedrordg@gmail.com>
2021-01-23 12:43:32 +0000

    initial structure finalized

---
+++ src/GuessWho.App/Startup.cs
@@ -0,0 +1,59 @@
+using GuessWho.Infrastructure.SignalR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GuessWho.App
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+
+            services.AddControllers();
+
+            services.AddSignalR()
+                    .AddAzureSignalR(options =>
+                    {
+                        options.ConnectionString = _configuration.GetConnectionString("SignalR");
+                        options.ApplicationName = "guesswho";
+                    });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+        }
+    }
+}
