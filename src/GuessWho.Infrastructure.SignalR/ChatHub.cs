f0a0f387cfd5f7cd52cf1e6330a82ac1a16101db
Pedro Rodrigues <pedrordg@gmail.com>
2021-01-24 18:06:10 +0000

    added contracts

--- src/GuessWho.Infrastructure.SignalR/ChatHub.cs
+++ src/GuessWho.Infrastructure.SignalR/ChatHub.cs
@@ -1,17 +1,19 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using GuessWho.SignalR.Contracts;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
 
 namespace GuessWho.Infrastructure.SignalR
 {
-    public class ChatHub : Hub
+    public class ChatHub : Hub<IChatClient>
     {
-        public void BroadcastMessage(string name, string message)
+        public async Task BroadcastMessage(string name, string message)
         {
-            Clients.All.SendAsync("broadcastMessage", name, message);
+            await Clients.All.BroadcastMessage( name, message);
         }
 
         public void Echo(string idolName)
         {
-            Clients.AllExcept(Context.ConnectionId).SendAsync("echo", idolName + " card flipped");
+            Clients.AllExcept(Context.ConnectionId).Echo(idolName + " card flipped");
         }
     }
 }
---
+++ src/GuessWho.SignalR.Contracts/IChatClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GuessWho.SignalR.Contracts
+{
+    public interface IChatClient
+    {
+        Task BroadcastMessage(string name, string message);
+
+        Task Echo(string idolName);
+    }
+}
